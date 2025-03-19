f9653f376f0d38b3f348c5cd765c8c4f96f61ec1
sonth <sonth@gemvietnam.com>
2025-03-19 16:07:13 +0700

    add neww component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/shared/components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from './core/shared/components/footer/footer.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AppRoutingModule, FooterComponent],
+  imports: [
+    AppRoutingModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/core/pages/home/homePage/homePage.component.ts
+++ src/app/core/pages/home/homePage/homePage.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductComponent } from '../../../shared/components/product/product.component';
 
 @Component({
   selector: 'app-homePage',
+  standalone: true,
   templateUrl: './homePage.component.html',
-  styleUrls: ['./homePage.component.scss']
+  styleUrls: ['./homePage.component.scss'],
+  imports: [
+    ProductComponent,
+  ]
 })
 export class HomePageComponent implements OnInit {
 
